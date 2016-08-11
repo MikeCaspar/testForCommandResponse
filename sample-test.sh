abcfec61c1635680b9abf1cd6fcac7ce6bd3d283
Mike Caspar <mike@caspar.com>
2016-08-11 18:04:03 -0400

    improve test script to return a 0 or 1

--- sample-test.sh
+++ sample-test.sh
@@ -1,19 +1,38 @@
 #!/usr/bin/env bash
 
-echo test Playbook is executing and recording results to log.txt
+echo "test Playbook is executing and recording results to log.txt"
 
 ansible-playbook tests/role.yml > tests/log.txt
 
-echo Passing Tests....
+echo "Passing Tests...."
 
-grep TEST_PASSED tests/log.txt
+grep "TEST_PASSED" tests/log.txt
 
-echo Failing Tests....
+varPassed="$(echo $?)"
 
-grep TEST_FAILED tests/log.txt
+echo "Serious Failures..."
 
-echo Serious Failures...
+grep "fatal" tests/log.txt
 
-grep fatal tests/log.txt
+echo $?
 
-exit 0
+echo "Failing Tests...."
+
+grep "TEST_FAILED" tests/log.txt
+
+varFailed="$(echo $?)"
+
+
+echo "Exit Code from Passed Tests: $varPassed"
+echo "Exit Code from Failed Tests: $varFailed"
+
+varResultCode="$varPassed$varFailed"
+varExpected="00"
+
+echo "Exit Code Result of Tests:$varResultCode:"
+
+if [ "$varResultCode" == "$varExpected" ]; then
+   exit 0
+else
+   exit 1
+fi
